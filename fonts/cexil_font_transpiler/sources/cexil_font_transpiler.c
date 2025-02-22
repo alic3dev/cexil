154cfd290c3de58805f400beecb41cbca3d618a3
alic3 <alic3dev@gmail.com>
2025-02-22 15:18:52 -0800

    cexil_font_transpiler_transpile: add

--- fonts/cexil_font_transpiler/sources/cexil_font_transpiler.c
+++ fonts/cexil_font_transpiler/sources/cexil_font_transpiler.c
@@ -1,10 +1,13 @@
 #include "cexil_font_transpiler.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "cexil_font_transpiler_intermediary.h"
+#include "cexil_font_transpiler_options.h"
 #include "cexil_font_transpiler_parser.h"
 #include "cexil_font_transpiler_print_usage.h"
+#include "cexil_font_transpiler_transpile.h"
 
 int main(int argc, char** argv) {
   if (argc != 3) {
@@ -16,30 +19,112 @@ int main(int argc, char** argv) {
     argv[1],
     "r"
   );
+  
+  unsigned int name_starting_index = 0;
+  
+  for (
+    unsigned int path_index = 0;
+    argv[1][path_index] != '\0';
+    ++path_index
+  ) {
+    name_starting_index = name_starting_index + 1;
+  }
+
+  for (
+    signed int path_index = name_starting_index;
+    path_index >= 0 && argv[1][path_index] != '/';
+    --path_index
+  ) {
+    name_starting_index = path_index;
+  }
+
+  char* name = malloc(1);
+  name[0] = '\0';
+
+  unsigned char has_extension = 0;
+  unsigned int extension_index = 0;
+  
+  for (
+    unsigned int path_index = name_starting_index;
+    argv[1][path_index] != '\0';
+    ++path_index
+  ) {
+    unsigned int name_index = path_index - name_starting_index;
+
+    name = realloc(
+      name,
+      sizeof(char) * (name_index + 2)
+    );
+    
+    name[name_index] = argv[1][path_index];
+    name[name_index + 1] = '\0';
+
+    if (name[name_index] == '.') {
+      has_extension = 1;
+      extension_index = name_index;
+    }
+  }
+
+  if (has_extension) {
+    name = realloc(
+      name,
+      sizeof(char) * (extension_index + 1)
+    );
+    
+    name[extension_index] = '\0';
+  }
 
   struct cexil_font_transpiler_intermediary* intermediary = (
     cexil_font_transpiler_parser_parse(
-      file_input_font
+      file_input_font,
+      name
     )
   );
 
+  free(name);
+
   fclose(file_input_font);
 
-  printf(
-    "character_set: %s\n"
-    "characters_count: %u\n"
-    "size_width: %u\n"
-    "size_height: %u\n",
-    intermediary->character_set,
-    intermediary->characters_count,
-    intermediary->size_width,
-    intermediary->size_height
-  );
+  int status_code_return = 0;
+
+  if (
+    intermediary->error ==
+    cexil_font_transpiler_intermediary_error_none
+  ) {
+    unsigned char transpile_status = cexil_font_transpiler_transpile(
+      intermediary,
+      argv[2]
+    );
+
+    if (transpile_status == 0) {
+      if (cexil_font_transpiler_option_display_basic_info != 0) {
+        cexil_font_transpiler_intermediary_print_basic_info(
+          intermediary
+        );
+      }
+    
+      if (cexil_font_transpiler_option_display_characters != 0) {
+        cexil_font_transpiler_intermediary_print_characters(
+          intermediary
+        );
+      }
+    } else {
+      fprintf(stderr, "Something went wrong while transpiling\n");
+      status_code_return = 1;
+    }
+  } else {
+    status_code_return = 1;
+    cexil_font_transpiler_intermediary_print_error(
+      intermediary
+    );
+  }
 
   cexil_font_transpiler_intermediary_destroy(
     intermediary
   );
+  
+  free(intermediary);
 
-  return 0;
+  return status_code_return;
 }
 
---
+++ fonts/cexil_font_transpiler/sources/cexil_font_transpiler_transpile.c
@@ -0,0 +1,516 @@
+#include "cexil_font_transpiler_transpile.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+const unsigned int __path_directory_output_final_sources_directory_length = 7;
+const unsigned int __path_directory_output_final_include_directory_length = 7;
+const unsigned int __path_directory_output_final_sources_file_extension_length = 2;
+const unsigned int __path_directory_output_final_include_file_extension_length = 2;
+
+const char* __path_directory_output_final_sources_directory = "sources";
+const char* __path_directory_output_final_include_directory = "include";
+const char* __path_directory_output_final_sources_file_extension = ".c";
+const char* __path_directory_output_final_include_file_extension = ".h";
+
+unsigned char cexil_font_transpiler_transpile(
+  struct cexil_font_transpiler_intermediary* intermediary,
+  char* path_directory_output
+) {
+  int mkdir_status = mkdir(
+    path_directory_output,
+    S_IRWXU | S_IRWXG | S_IRWXO
+  );
+  
+  if (mkdir_status != 0 && errno != EEXIST) {
+    fprintf(
+      stderr,
+      "unable_to_create_directory: %s\n",
+      path_directory_output
+    );
+    return 1;
+  }
+  
+  unsigned int path_directory_output_final_length = 2;
+  char* path_directory_output_final = malloc(
+    sizeof(char) * path_directory_output_final_length
+  );
+  
+  for (
+    unsigned int path_index = 0;
+    path_directory_output[path_index] != '\0';
+    ++path_index
+  ) {
+    path_directory_output_final_length = path_directory_output_final_length + 1;
+
+    path_directory_output_final = realloc(
+      path_directory_output_final,
+      sizeof(char) * path_directory_output_final_length
+    );
+
+    path_directory_output_final[path_index] = (
+      path_directory_output[path_index]
+    );
+  }
+  
+  path_directory_output_final[
+    path_directory_output_final_length - 2
+  ] = '/';
+
+  unsigned int name_length = 0;
+
+  for (
+    unsigned int name_index = 0;
+    intermediary->name[name_index] != '\0';
+    ++name_index
+  ) {
+    name_length = name_length + 1;
+    path_directory_output_final_length = path_directory_output_final_length + 1;
+    
+    path_directory_output_final = realloc(
+      path_directory_output_final,
+      sizeof(char) * path_directory_output_final_length
+    );
+
+    path_directory_output_final[path_directory_output_final_length - 2] = (
+      intermediary->name[name_index]
+    );
+  }
+  
+  path_directory_output_final[
+    path_directory_output_final_length - 1
+  ] = '\0';
+
+  mkdir_status = mkdir(
+    path_directory_output_final,
+    S_IRWXU | S_IRWXG | S_IRWXO
+  );
+
+  if (mkdir_status != 0 && errno != EEXIST) {
+    fprintf(
+      stderr,
+      "unable_to_create_directory: %s\n",
+      path_directory_output_final
+    );
+    
+    free(path_directory_output_final);
+    return 1;
+  }
+
+  unsigned int path_directory_output_final_sources_directory_length = (
+    path_directory_output_final_length + __path_directory_output_final_sources_directory_length + 1
+  );
+  unsigned int path_directory_output_final_include_directory_length = (
+    path_directory_output_final_length + __path_directory_output_final_include_directory_length + 1
+  );
+  unsigned int path_directory_output_final_sources_file_length = (
+    path_directory_output_final_sources_directory_length + 
+    name_length + 
+    __path_directory_output_final_sources_file_extension_length +
+    2
+  );
+  unsigned int path_directory_output_final_include_file_length = (
+    path_directory_output_final_include_directory_length + 
+    name_length + 
+    __path_directory_output_final_include_file_extension_length +
+    2
+  );
+
+  char* path_directory_output_final_sources_directory = malloc(
+    sizeof(char) * path_directory_output_final_sources_directory_length
+  );
+  char* path_directory_output_final_include_directory = malloc(
+    sizeof(char) * path_directory_output_final_include_directory_length
+  );
+  char* path_directory_output_final_sources_file = malloc(
+    sizeof(char) * path_directory_output_final_sources_file_length
+  );
+  char* path_directory_output_final_include_file = malloc(
+    sizeof(char) * path_directory_output_final_include_file_length
+  );
+
+  for (
+    unsigned int path_index = 0;
+    path_index < path_directory_output_final_length;
+    ++path_index
+  ) {
+    char path_char = path_directory_output_final[path_index];
+
+    path_directory_output_final_sources_directory[path_index] = path_char;
+    path_directory_output_final_include_directory[path_index] = path_char;
+    path_directory_output_final_sources_file[path_index] = path_char;
+    path_directory_output_final_include_file[path_index] = path_char;
+  }
+  
+  path_directory_output_final_sources_directory[
+    path_directory_output_final_length - 1
+  ] = '/';
+  path_directory_output_final_include_directory[
+    path_directory_output_final_length - 1
+  ] = '/';
+  path_directory_output_final_sources_file[
+    path_directory_output_final_length - 1
+  ] = '/';
+  path_directory_output_final_include_file[
+    path_directory_output_final_length - 1
+  ] = '/';
+
+  for (
+    unsigned int sources_directory_index = 0;
+    sources_directory_index < __path_directory_output_final_sources_directory_length;
+    ++sources_directory_index
+  ) {
+    char sources_directory_char = __path_directory_output_final_sources_directory[
+      sources_directory_index
+    ];
+    unsigned int path_index = (
+      path_directory_output_final_length +
+      sources_directory_index
+    );
+
+    path_directory_output_final_sources_directory[
+      path_index
+    ] = sources_directory_char;
+
+    path_directory_output_final_sources_file[
+      path_index
+    ] = sources_directory_char;
+  }
+  
+  for (
+    unsigned int include_directory_index = 0;
+    include_directory_index < __path_directory_output_final_include_directory_length;
+    ++include_directory_index
+  ) {
+    char include_directory_char = __path_directory_output_final_include_directory[
+      include_directory_index
+    ];
+    unsigned int path_index = (
+      path_directory_output_final_length +
+      include_directory_index
+    );
+
+    path_directory_output_final_include_directory[
+      path_index
+    ] = include_directory_char;
+
+    path_directory_output_final_include_file[
+      path_index
+    ] = include_directory_char;
+  }
+
+  path_directory_output_final_sources_file[
+    path_directory_output_final_sources_directory_length - 1
+  ] = '/';
+
+  path_directory_output_final_include_file[
+    path_directory_output_final_include_directory_length - 1
+  ] = '/';
+ 
+  char* name_upper = malloc(sizeof(char) * (name_length + 1));
+  char* name_lower = malloc(sizeof(char) * (name_length + 1));
+
+  for (
+    unsigned int name_index = 0;
+    name_index < name_length;
+    ++name_index
+  ) {
+    char name_upper_char = intermediary->name[name_index];
+    char name_lower_char = name_upper_char;
+
+    if (name_upper_char >= 'a' && name_upper_char <= 'z') {
+      name_upper_char = name_upper_char + ('A' - 'a');
+    } else if (name_lower_char >= 'A' && name_lower_char <= 'Z') {
+      name_lower_char = name_lower_char - ('A' - 'a');
+    } else if (name_upper_char == ' ') {
+      name_upper_char = '_';
+      name_lower_char = '_';
+    }
+
+    name_upper[name_index] = name_upper_char;
+    name_lower[name_index] = name_lower_char;
+  }
+  
+  name_upper[name_length] = '\0';
+  name_lower[name_length] = '\0';
+  
+  for (
+    unsigned int name_index = 0;
+    name_index < name_length;
+    ++name_index
+  ) {
+    path_directory_output_final_sources_file[
+      path_directory_output_final_sources_directory_length +
+      name_index 
+    ] = name_lower[name_index];
+    
+    path_directory_output_final_include_file[
+      path_directory_output_final_include_directory_length +
+      name_index 
+    ] = name_lower[name_index];
+  }
+
+  for (
+    unsigned int extension_index = 0;
+    extension_index < __path_directory_output_final_sources_file_extension_length;
+    ++extension_index
+  ) {
+    path_directory_output_final_sources_file[
+      path_directory_output_final_sources_file_length - 
+      (__path_directory_output_final_sources_file_extension_length - extension_index) -
+      2
+    ] = __path_directory_output_final_sources_file_extension[extension_index];
+  }
+   
+  for (
+    unsigned int extension_index = 0;
+    extension_index < __path_directory_output_final_include_file_extension_length;
+    ++extension_index
+  ) {
+    path_directory_output_final_include_file[
+      path_directory_output_final_include_file_length - 
+      (__path_directory_output_final_include_file_extension_length - extension_index) -
+      2
+    ] = __path_directory_output_final_include_file_extension[extension_index];
+  }
+
+  path_directory_output_final_sources_directory[
+    path_directory_output_final_sources_directory_length - 1
+  ] = '\0';
+  path_directory_output_final_include_directory[
+    path_directory_output_final_include_directory_length - 1
+  ] = '\0';
+  path_directory_output_final_sources_file[
+    path_directory_output_final_sources_file_length - 1
+  ] = '\0';
+  path_directory_output_final_include_file[
+    path_directory_output_final_include_file_length - 1
+  ] = '\0';
+ 
+  mkdir_status = mkdir(
+    path_directory_output_final_sources_directory,
+    S_IRWXU | S_IRWXG | S_IRWXO
+  );
+
+  if (mkdir_status != 0 && errno != EEXIST) {
+    fprintf(
+      stderr,
+      "unable_to_create_directory: %s\n",
+      path_directory_output_final_sources_directory
+    );
+
+    free(name_upper);
+    free(name_lower);
+    free(path_directory_output_final);
+    free(path_directory_output_final_sources_directory);
+    free(path_directory_output_final_include_directory);
+    free(path_directory_output_final_sources_file);
+    free(path_directory_output_final_include_file);
+    
+    return 1;
+  }
+
+  mkdir_status = mkdir(
+    path_directory_output_final_include_directory,
+    S_IRWXU | S_IRWXG | S_IRWXO
+  );
+
+  if (mkdir_status != 0 && errno != EEXIST) {
+    fprintf(
+      stderr,
+      "unable_to_create_directory: %s\n",
+      path_directory_output_final_include_directory
+    );
+
+    free(name_upper);
+    free(name_lower); 
+    free(path_directory_output_final);
+    free(path_directory_output_final_sources_directory);
+    free(path_directory_output_final_include_directory);
+    free(path_directory_output_final_sources_file);
+    free(path_directory_output_final_include_file);
+    
+    return 1;
+  }
+  
+  FILE* file_directory_output_final_sources_file = fopen(
+    path_directory_output_final_sources_file,
+    "w"
+  );
+
+  if (file_directory_output_final_sources_file == (void*)0) {
+    fprintf(
+      stderr,
+      "unable_to_open_for_writing: %s\n",
+      path_directory_output_final_sources_file
+    );
+    
+    free(name_upper);
+    free(name_lower);
+    free(path_directory_output_final);
+    free(path_directory_output_final_sources_directory);
+    free(path_directory_output_final_include_directory);
+    free(path_directory_output_final_sources_file);
+    free(path_directory_output_final_include_file);
+    
+    return 1;
+  }
+
+  fprintf(
+    file_directory_output_final_sources_file,
+    "#include \"%s.h\"\n\n"
+    "const char cexil_font_definition_%s_characters[\n"
+    "  CEXIL_FONT_DEFINITION_%s_CHARACTERS_COUNT\n"
+    "][\n"
+    "  CEXIL_FONT_DEFINITION_%s_SIZE_HEIGHT\n"
+    "][\n"
+    "  CEXIL_FONT_DEFINITION_%s_SIZE_WIDTH\n"
+    "] = {",
+    name_lower,
+    name_lower,
+    name_upper,
+    name_upper,
+    name_upper
+  );
+
+  for (
+    unsigned int character_index = 0;
+    character_index < intermediary->characters_count;
+    ++character_index
+  ) {
+    fprintf(
+      file_directory_output_final_sources_file,
+      "%s {",
+      character_index > 0 ? "," : "\n "
+    );
+  
+    for (
+      unsigned int y_index = 0;
+      y_index < intermediary->size_height;
+      ++y_index
+    ) {
+      fprintf(
+        file_directory_output_final_sources_file,
+        "%s\n    {",
+        y_index > 0 ? "," : ""
+      );
+
+      for (
+        unsigned int x_index = 0;
+        x_index < intermediary->size_width;
+        ++x_index
+      ) {
+        fprintf(
+          file_directory_output_final_sources_file,
+          "%s %u",
+          x_index > 0 ? "," : "",
+          intermediary->characters[
+            character_index
+          ][y_index][x_index]
+        );
+      }
+
+      fprintf(
+        file_directory_output_final_sources_file,
+        " }"
+      );
+    }
+
+    fprintf(
+      file_directory_output_final_sources_file,
+      "\n  }"
+    );
+  }
+
+  fprintf(
+    file_directory_output_final_sources_file,
+    "\n};\n\n"
+    "const struct cexil_font cexil_font_definition_%s = {\n"
+    "  character_set: \"%s\","
+    "  characters: (char**)(\n"
+    "    cexil_font_definition_%s_characters\n"
+    "  ),\n"
+    "  size: {\n"
+    "    width: CEXIL_FONT_DEFINITION_%s_SIZE_WIDTH,\n"
+    "    height: CEXIL_FONT_DEFINITION_%s_SIZE_HEIGHT\n"
+    "  }\n"
+    "};\n\n",
+    name_lower,
+    intermediary->character_set,
+    name_lower,
+    name_upper,
+    name_upper
+  );
+
+  FILE* file_directory_output_final_include_file = fopen(
+    path_directory_output_final_include_file,
+    "w"
+  );
+
+  if (file_directory_output_final_include_file == (void*)0) {
+    fprintf(
+      stderr,
+      "unable_to_open_for_writing: %s\n",
+      path_directory_output_final_include_file
+    );
+
+    fclose(file_directory_output_final_sources_file);
+    
+    free(name_upper);
+    free(name_lower);
+    free(path_directory_output_final);
+    free(path_directory_output_final_sources_directory);
+    free(path_directory_output_final_include_directory);
+    free(path_directory_output_final_sources_file);
+    free(path_directory_output_final_include_file);
+    
+    return 1;
+  }
+
+  fprintf(
+    file_directory_output_final_include_file,
+    "#ifndef __CEXIL_FONT_DEFINITION_%s_H\n"
+    "#define __CEXIL_FONT_DEFINITION_%s_H\n\n"
+    "#define CEXIL_FONT_DEFINITION_%s_CHARACTERS_COUNT %u\n"
+    "#define CEXIL_FONT_DEFINITION_%s_SIZE_WIDTH %u\n"
+    "#define CEXIL_FONT_DEFINITION_%s_SIZE_HEIGHT %u\n\n"
+    "extern const char cexil_font_definition_%s_characters[\n"
+    "  CEXIL_FONT_DEFINITION_%s_CHARACTERS_COUNT\n"
+    "][\n"
+    "  CEXIL_FONT_DEFINITION_%s_SIZE_HEIGHT\n"
+    "][\n"
+    "  CEXIL_FONT_DEFINITION_%s_SIZE_WIDTH\n"
+    "];\n\n"
+    "extern const struct cexil_font cexil_font_definition_%s;\n\n"
+    "#endif\n\n",
+    name_upper,
+    name_upper,
+    name_upper,
+    intermediary->characters_count,
+    name_upper,
+    intermediary->size_width,
+    name_upper,
+    intermediary->size_height,
+    name_lower,
+    name_upper,
+    name_upper,
+    name_upper,
+    name_lower
+  );
+
+  fclose(file_directory_output_final_sources_file);
+  fclose(file_directory_output_final_include_file);
+
+  free(name_upper);
+  free(name_lower);
+  free(path_directory_output_final);
+  free(path_directory_output_final_sources_directory);
+  free(path_directory_output_final_include_directory);
+  free(path_directory_output_final_sources_file);
+  free(path_directory_output_final_include_file);
+ 
+  return 0;
+}
+
