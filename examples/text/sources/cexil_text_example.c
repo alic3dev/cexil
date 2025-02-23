8f452c97e644fb018275762821b2c5fb3ef1046e
alic3 <alic3dev@gmail.com>
2025-02-22 20:26:34 -0800

    examples/text: add

---
+++ examples/text/sources/cexil_text_example.c
@@ -0,0 +1,68 @@
+#include "cexil_text_example.h"
+
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <time.h>
+
+#include <stdio.h>
+
+#include "cexil.h"
+
+unsigned char interupted = 0;
+
+int main() {
+  struct sigaction signal_action;
+  signal_action.sa_handler = interupt_handler;
+  sigaction(SIGINT, &signal_action, NULL);
+  
+  struct winsize terminal_size;
+
+  srand(time(NULL));
+
+  if (
+    ioctl(0, TIOCGWINSZ, &terminal_size) == -1
+  ) {
+    return 1;
+  }
+
+  struct cexil_renderer renderer;
+  struct cexil_size size = {
+    width: (terminal_size.ws_col - 1) * 2,
+    height: (terminal_size.ws_row - 1) * 4
+  };
+
+  cexil_renderer_initialize(
+    &renderer,
+    &size
+  );
+
+  struct cexil_text text;
+  cexil_text_initialize(
+    &text,
+    "abcdefghijklmnopqrstuvwxyz"
+  );
+
+  cexil_renderer_text_add(
+    &renderer,
+    &text
+  );
+
+  struct timespec frame_time = { 0, 25000000 };
+  
+  while (interupted == 0) {
+    nanosleep(&frame_time, NULL);
+
+    cexil_renderer_render_clear(&renderer);
+    cexil_renderer_render(&renderer);
+  }
+  
+  cexil_renderer_destroy(&renderer);
+ 
+  return 0;
+}
+
+void interupt_handler(int _) {
+  interupted = 1;
+}
+
