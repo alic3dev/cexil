65adc66244e63f5f12ef329d2c594bce73a01266
alic3 <alic3dev@gmail.com>
2025-02-22 19:37:57 -0800

    cexil_font: char*** -> char*

--- include/cexil_font.h
+++ include/cexil_font.h
@@ -4,12 +4,9 @@
 #include "cexil_character_set.h"
 #include "cexil_size.h"
 
-#define CEXIL_FONT_SIZE_WIDTH_DEFAULT 6
-#define CEXIL_FONT_SIZE_HEIGHT_DEFAULT 4
-
 struct cexil_font {
   enum cexil_character_set character_set;
-  char*** characters;
+  char* characters;
   struct cexil_size size;
 };
 
