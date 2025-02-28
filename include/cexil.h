c5cf8b4f46d50ae8201aa78e2da1dad6d4452c49
alic3dev <alic3dev@gmail.com>
2025-02-28 05:59:32 +0000

    `include/cexil.h`:add_missing_includes

--- include/cexil.h
+++ include/cexil.h
@@ -2,12 +2,15 @@
 #define __CEXIL_H
 
 #include "cexil_blend_mode.h"
+#include "cexil_character_set.h"
 #include "cexil_collision.h"
+#include "cexil_font.h"
 #include "cexil_pixel_mapping.h"
 #include "cexil_position.h"
 #include "cexil_renderer.h"
 #include "cexil_size.h"
 #include "cexil_sprite.h"
+#include "cexil_text.h"
 #include "cexil_time.h"
 #include "cexil_timer.h"
 
