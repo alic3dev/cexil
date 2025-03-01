7000a7904c81213a82dfa3167eaa3e81f1553bde
alic3 <alic3dev@gmail.com>
2025-03-01 13:07:24 -0800

    `cexil_text_example`:wrap:on|off

--- examples/text/include/cexil_text_example.h
+++ examples/text/include/cexil_text_example.h
@@ -3,6 +3,9 @@
 
 extern unsigned char interupted;
 
+extern char string_wrap_on[8];
+extern char string_wrap_off[9];
+
 int main();
 
 void interupt_handler(int);
--- examples/text/sources/cexil_text_example.c
+++ examples/text/sources/cexil_text_example.c
@@ -11,6 +11,9 @@
 
 unsigned char interupted = 0;
 
+char string_wrap_on[8] = "wrap on\0";
+char string_wrap_off[9] = "wrap off\0";
+
 int main() {
   struct sigaction signal_action;
   signal_action.sa_handler = interupt_handler;
@@ -61,10 +64,39 @@ int main() {
     &text_long
   );
 
-  struct timespec frame_time = { 0, 25000000 };
-  
+  struct cexil_text text_wrap;
+  cexil_text_initialize(
+    &text_wrap,
+    text_wrap.wrap == 1 ? string_wrap_on : string_wrap_off
+  );
+
+  text_wrap.position.y = renderer.size.height - text_wrap.font->size.height;
+
+  cexil_renderer_text_add(
+    &renderer,
+    &text_wrap
+  );
+
+  struct cexil_time time_frame;
+  cexil_time_initialize(&time_frame, 0, 25000000);
+
+  struct cexil_timer timer_wrap;
+  cexil_timer_start(&timer_wrap); 
+ 
   while (interupted == 0) {
-    nanosleep(&frame_time, NULL);
+    cexil_time_sleep(&time_frame);
+
+    if (
+      cexil_timer_time_elapsed(&timer_wrap) > 2000000
+    ) {
+      cexil_timer_start(&timer_wrap);
+      
+      text_long.wrap = text_long.wrap == 0 ? 1 : 0;
+      cexil_text_text_set(
+        &text_wrap,
+        text_long.wrap == 0 ? string_wrap_off : string_wrap_on
+      );
+    }
 
     cexil_renderer_render_clear(&renderer);
     cexil_renderer_render(&renderer);
